f0f8ab2f48795b4242d096b3b11cb62da268956e
Turalyusubov <tu201906152@code.edu.az>
2023-04-05 19:23:49 +0400

    updated hero banner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,18 +3,22 @@ import HeroBannerImage from './../assets/images/HeroBannerImage.jpg'
 
 export default function HeroBanner() {
     return (
-        <div className='flex justify-between p-12'>
-            <div className="hero-left">
+        <div className='p-4 sm:p-12 text-center md:text-left'>
+            <div className="hero-left flex flex-col">
                 <p className='text-orange-600 font-bold text-xl mb-4'>FitZone</p>
-                <h2 className='font-bold text-4xl mb-6'>
-                    Sweat, smile <br />
+                <h2 className='font-bold md:w-4/12 text-4xl mb-6 overflow-hidden'>
+                    Sweat, smile
                     and repeat
                 </h2>
                 <p className='mb-4'>Check out the most effective exercises</p>
-                <button className='rounded bg-orange-600 text-yellow-50 px-4 py-2 uppercase'>Explore exercises</button>
+                <button className='self-center md:self-auto sm:w-1/2 md:w-4/12 rounded bg-orange-600 hover:bg-orange-700 
+                transition duration-300 text-yellow-50 px-4 py-2 
+                uppercase'>
+                    Explore exercises</button>
             </div>
-            <div className="hero-right w-5/12 absolute top-0 right-12">
-                <img className='w-full' src={HeroBannerImage} alt="HeroBanner image" />
+            <span className='hidden md:block w-full text-amber-100 text-9xl font-bold mt-10'>Exercise</span>
+            <div className="hero-right w-5/12 hidden md:block absolute top-0 right-12">
+                <img className='w-full h-full rounded-bl-3xl' src={HeroBannerImage} alt="HeroBanner image" />
             </div>
         </div>
     )
